f91c1d825f65c0ee68650850df02da08719a79e4
“Amon” <amonbali0925@gmail.com>
2018-10-13 01:11:18 -0400

    1:11

---
+++ update_requirements.py
@@ -0,0 +1,10 @@
+import pkg_resources
+
+#Write the modules installed in the current virtual env into requirements.txt
+modules = [module for module in pkg_resources.working_set]
+with open("requirements.txt", "w") as file:
+    content = ""
+    for i in range(len(modules)):
+        content += (str(modules[i]).split(" ")[0] + "==" + str(modules[i]).split(" ")[1]) + "\n"
+    file.write(content)
+
