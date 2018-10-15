410e4f9c157ea2d00db7cef71be0f0316bad38b7
“Amon” <amonbali0925@gmail.com>
2018-10-15 07:31:39 -0400

    Deployed by command line

--- project_base/settings.py
+++ project_base/settings.py
@@ -17,7 +17,9 @@ import django_heroku
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
+
 # Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -124,11 +126,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
 
-STATIC_URL = '/static/'
+    ('assets', STATIC_ROOT),
+)
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 #WhiteNoise static file caching storage settings. See: http://whitenoise.evans.io/en/stable/django.html#django-middleware
