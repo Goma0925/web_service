3f7f69972f6bfc1635ce51734566d87ce02cb6ec
“Amon” <amonbali0925@gmail.com>
2018-11-05 07:43:54 -0500

    11/5

--- applications/events/views.py
+++ applications/events/views.py
@@ -5,8 +5,9 @@ from datetime import datetime
 def eventboard(request):
     #Sort by nearest upcoming events
     future_events = Event.objects.filter(date__gte=datetime.now())
+    print("Num of events:", len(future_events))
     for event in future_events:
         event.date = str(event.date.month) + "/" + str(event.date.day) + "/" + str(event.date.year)
         print(event.date)
 
-    return render(request, "eventboard/eventboard.html", {"events": future_events})
+    return render(request, "eventboard/index.html", {"events": future_events})
